e906d7cb937875d42c3ad45ab24ca6ed320f922f
MaksGorobets <gorobets542@gmail.com>
2023-10-25 04:49:33 +0300

    Initial commit for RPS

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,24 +1,85 @@
-//
-//  ContentView.swift
-//  RockPaperScissors
-//
-//  Created by Maks Winters on 25.10.2023.
-//
-
 import SwiftUI
 
+struct CustomColor {
+    static let myColor = Color("Color")
+    // Add more here...
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    let RPC = ["🪨", "📃", "✂️"]
+    
+    enum Outcome {
+        case win
+        case lose
+        case tie
+    }
+    
+    @State private var displayedImage = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    
+    func winChecker(guess: String, displayedImage: String) -> Outcome {
+        if guess == displayedImage {
+            return Outcome.tie
+        } else if guess == "🪨" {
+            if displayedImage == "📃" {
+                return Outcome.lose
+            } else if displayedImage == "✂️" {
+                return Outcome.win
+            }
+        } else if guess == "📃" {
+            if displayedImage == "✂️" {
+                return Outcome.lose
+            } else if displayedImage == "🪨" {
+                return Outcome.win
+            }
+        } else if guess == "✂️" {
+            if displayedImage == "🪨" {
+                return Outcome.lose
+            } else if displayedImage == "📃" {
+                return Outcome.win
+            }
         }
-        .padding()
+        return Outcome.tie
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Score: \(score)")
+                Spacer()
+                Text(RPC[displayedImage])
+                    .font(.system(size: 150))
+                Spacer()
+                HStack(spacing: 30) {
+                    ForEach(RPC, id: \.self) { item in
+                        Button(item) {
+                            if winChecker(guess: item, displayedImage: RPC[displayedImage]) == (shouldWin ? .win : .lose) {
+                                score += 100
+                            } else {
+                                score -= 100
+                            }
+                            displayedImage = Int.random(in: 0...2)
+                            shouldWin.toggle()                        }
+                    }
+                }
+                .font(.system(size: 50))
+                Spacer()
+                Text("Your objective is to: \(shouldWin ? "Win" : "Lose")")
+                    .foregroundStyle(.white)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(CustomColor.myColor.gradient)
+            .navigationTitle("RockPaperScissors")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
