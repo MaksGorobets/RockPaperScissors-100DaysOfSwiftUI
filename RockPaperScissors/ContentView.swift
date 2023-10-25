fee15cfb2003db588920dbf08abb685a15a1bb8d
MaksGorobets <gorobets542@gmail.com>
2023-10-25 16:27:35 +0300

    Alert update, 10 games limit, other updates

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     @State private var displayedImage = Int.random(in: 0...2)
     @State private var shouldWin = Bool.random()
     @State private var score = 0
+    @State private var gamesPlayed = 0
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     
     func winChecker(guess: String, displayedImage: String) -> Outcome {
         if guess == displayedImage {
@@ -44,6 +47,33 @@ struct ContentView: View {
         return Outcome.tie
     }
     
+    func resetGame() {
+        showAlert = false
+        score = 0
+        gamesPlayed = 0
+        displayedImage = Int.random(in: 0...2)
+        shouldWin.toggle()
+    }
+    
+    func buttonPress(item: String) {
+        gamesPlayed += 1
+        if winChecker(guess: item, displayedImage: RPC[displayedImage]) == (shouldWin ? .win : .lose) {
+            score += 100
+        } else {
+            score -= 100
+        }
+        displayedImage = Int.random(in: 0...2)
+        shouldWin.toggle()
+        if gamesPlayed >= 10 {
+            showAlert = true
+        }
+        if score >= 500 {
+            alertMessage = "Good score!"
+        } else {
+            alertMessage = "Good game."
+        }
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -55,19 +85,19 @@ struct ContentView: View {
                 HStack(spacing: 30) {
                     ForEach(RPC, id: \.self) { item in
                         Button(item) {
-                            if winChecker(guess: item, displayedImage: RPC[displayedImage]) == (shouldWin ? .win : .lose) {
-                                score += 100
-                            } else {
-                                score -= 100
-                            }
-                            displayedImage = Int.random(in: 0...2)
-                            shouldWin.toggle()                        }
+                            buttonPress(item: item)
+                        }
                     }
                 }
                 .font(.system(size: 50))
                 Spacer()
                 Text("Your objective is to: \(shouldWin ? "Win" : "Lose")")
                     .foregroundStyle(.white)
+            }
+            .alert(alertMessage, isPresented: $showAlert) {
+                Button("New Game", action: resetGame)
+            } message: {
+                Text("Your final score is \(score)")
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(CustomColor.myColor.gradient)
